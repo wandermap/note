7617843aacd08c73fe8427a5a79144e76b4b531e
wandermap <the7pieceplot@gmail.com>
2021-06-20 21:54:18 +0900

    選択問題を追加

---
+++ shcool/commpornent.js
@@ -0,0 +1,51 @@
+Vue.component("navbar", {
+  template: `
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-secondary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="../index.html">Navbar</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+
+          <!-- 右サイド -->
+          <!-- <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form> -->
+        </div>
+      </div>
+    </nav>
+`,
+});
+Vue.component("sidemenu", {
+  template: `
+  <ul class="col-md-2 sidemenu" style="list-style: none">
+    <li><a href="01_レクチャー.html">レクチャー</a></li>
+    <li><a href="02_選択問題.html">選択問題</a></li>
+  </ul>
+  `,
+});
